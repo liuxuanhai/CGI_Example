f01ee2109b2b0ee45df4ddda593c463178c14586
Mr.Yu <astralrovers@outlook.com>
2018-05-17 21:41:51 +0800

    verification_center

---
+++ projs/cloud-server-management-system/verification_center/app/main.cpp
@@ -0,0 +1,22 @@
+/******************************************    *******************************
+
+      > File Name: main.cpp
+
+      > Author: Ywl
+
+      > Descripsion:
+
+      > Created Time:     Sat 12 May 2018 12:13:01 PM CST
+
+      > Modify Time: 
+
+ *********************************    ***************************************/
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << ".................star verification center....................." << endl;
+}
