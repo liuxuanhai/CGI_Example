829064218c0f0f28f4bdab2593ea2fdc399766f8
Mr.Yu <astralrovers@outlook.com>
2018-05-17 03:19:56 -0700

    encryption

--- c++/encry/use_openssl.cpp
+++ c++/encry/use_openssl.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+#include <stdlib.h>
 #include <string.h>
 
-#include <openssl/md5.h>
+#include "encryption.h"
 
 using namespace std;
+
 
 int main(int argc, char **argv)
 {
 	unsigned char md[33] = {0};
 	string pass = "password";
-	MD5((const unsigned char *)pass.c_str(), pass.length(), md);
 
-	char key[65];
-	char tmp[3];
-	for(int i = 0; i < 32; i++)
-	{
-		sprintf(tmp, "%02x", md[i]);
-		strcat(key, tmp);
-	}
-	key[32] = '\0';
-	cout << key << endl;
+	md5((const unsigned char *)pass.c_str(), pass.length(), md);
+
+	printf("md5: %s\n", md);
+
+	memset(md, 0, sizeof(md));
+	sha256((const unsigned char *)pass.c_str(), pass.length(), md);
+
+	printf("sha256: %s\n", md);
 	return 0;
 }
 /* g++ use_openssl.cpp -lcrypto -lssl */
